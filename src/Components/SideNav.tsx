986c34fbb8366e3282c9acd4182c932ce33a92fa
Gabriel Fernando <gf313074@gmail.com>
2024-12-20 18:57:24 -0300

    create side navigation and styles

--- src/Components/SideNav.tsx
+++ src/Components/SideNav.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
-
+import resume from "../assets/icons/resumo.svg";
+import sale from "../assets/icons/vendas.svg";
+import webhooks from "../assets/icons/webhooks.svg";
+import configuration from "../assets/icons/configuracoes.svg";
+import contact from "../assets/icons/contato.svg";
+import out from "../assets/icons/sair.svg";
+import FintechSVG from "../assets/FintechSVG";
 const SideNav = () => {
   return (
-    <nav>SideNav</nav>
-  )
-}
+    <nav className="sidenav box bg-3">
+      <FintechSVG title="Fintech Logo"/>
+      <ul>
+        <li>
+          <span>
+            <img src={resume} alt="" />
+          </span>
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <span>
+            <img src={sale} alt="" />
+          </span>
+          <a href="">Vendas</a>
+        </li>
+        <li>
+          <span>
+            <img src={webhooks} alt="" />
+          </span>
+          <a>Webkooks</a>
+        </li>
+        <li>
+          <span>
+            <img src={configuration} alt="" />
+          </span>
+          <a>Configurações</a>
+        </li>
+        <li>
+          <span>
+            <img src={contact} alt="" />
+          </span>
+          <a>Contato</a>
+        </li>
+        <li>
+          <span>
+            <img src={out} alt="" />
+          </span>
+          <a>Sair</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default SideNav
+export default SideNav;
